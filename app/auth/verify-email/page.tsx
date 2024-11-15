12efe297b9b72610525f459abd63e06855bee6f7
Yaniv <yanivdouieb@icloud.com>
2024-11-15 22:38:09 +0100

    Update email verification message and adjust email address access in verification component

--- app/auth/verify-email/page.tsx
+++ app/auth/verify-email/page.tsx
@@ -8,17 +8,14 @@ import apiClient from "@/utils/apiClient";
 const AuthVerifyEmail = (): React.ReactElement => {
   const [email, setEmail] = useState<string>("");
   useEffect(() => {
-    apiClient
-      .get("/auth/me")
-      .then((response) => {
-        setEmail(response.data.data.email);
-      })
-      .catch(() => {});
+    apiClient.get("/auth/me").then((response) => {
+      setEmail(response.data.data.email.address);
+    });
   }, []);
   return (
     <Stack>
       <Title order={2}>{"Vérifier votre adresse e-mail"}</Title>
-      <Text>{`Consultez la boîte de réception de ${email} pour vérifier votre compte et démarrer.`}</Text>
+      <Text>{`Consultez la boîte de réception de ${email} pour vérifier votre compte et démarrer sur GoBiblio.`}</Text>
       <Group>
         <Button>{"Renvoyer l'e-mail"}</Button>
       </Group>
