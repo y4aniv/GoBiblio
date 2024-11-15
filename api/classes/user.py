0cb6bc0f951a560aaef0ad4e14aeb288d6c9f566
Yaniv <120222659+y4aniv@users.noreply.github.com>
2024-11-15 11:01:58 +0000

    Add User model with attributes and initialization method

---
+++ api/classes/user.py
@@ -0,0 +1,38 @@
+from sqlalchemy import DateTime, Index, Text, text
+from sqlalchemy.orm import mapped_column
+from utils.orm import Base
+
+class User(Base):
+    __tablename__ = 'User'
+    __table_args__ = (
+        Index('User_email_key', 'email', unique=True),
+    )
+
+    id = mapped_column(Text, primary_key=True)
+    first_name = mapped_column(Text, nullable=False)
+    last_name = mapped_column(Text, nullable=False)
+    email = mapped_column(Text, nullable=False)
+    password = mapped_column(Text, nullable=False)
+    created_at = mapped_column(DateTime, nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+    updated_at = mapped_column(DateTime, nullable=False)
+
+    def __init__(self, id, first_name, last_name, email, password, created_at, updated_at):
+        """
+        Initialize the User object
+
+        Args:
+            - id (str): The id of the user
+            - first_name (str): The first name of the user
+            - last_name (str): The last name of the user
+            - email (str): The email of the user
+            - password (str): The password of the user
+            - created_at (datetime): The time the user was created
+            - updated_at (datetime): The time the user was updated
+        """
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.created_at = created_at
+        self.updated_at = updated_at
