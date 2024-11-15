3fe82e43c91021f3ffa5a8f5c83e1a2ad5d39338
Yaniv <yanivdouieb@icloud.com>
2024-11-15 20:29:30 +0100

    Fetch and display user's email in verification message

--- app/auth/verify-email/page.tsx
+++ app/auth/verify-email/page.tsx
@@ -1,12 +1,24 @@
+"use client";
+
 import { Button, Group, Stack, Text, Title } from "@mantine/core";
+import { useEffect, useState } from "react";
+
+import apiClient from "@/utils/apiClient";
 
 const AuthVerifyEmail = (): React.ReactElement => {
+  const [email, setEmail] = useState<string>("");
+  useEffect(() => {
+    apiClient
+      .get("/auth/me")
+      .then((response) => {
+        setEmail(response.data.data.email);
+      })
+      .catch(() => {});
+  }, []);
   return (
     <Stack>
       <Title order={2}>{"Vérifier votre adresse e-mail"}</Title>
-      <Text>
-        {"Consultez la boîte de réception de bruno.cordier@example.com pour vérifier votre compte et démarrer."}
-      </Text>
+      <Text>{`Consultez la boîte de réception de ${email} pour vérifier votre compte et démarrer.`}</Text>
       <Group>
         <Button>{"Renvoyer l'e-mail"}</Button>
       </Group>
