219949b911c9131b6eb737303282537b47525d6d
Yaniv <yanivdouieb@icloud.com>
2024-11-15 18:15:44 +0100

    Add data type checking utility function to validate request data types

--- api/utils/lib.py
+++ api/utils/lib.py
@@ -15,3 +15,19 @@ def find_missing_keys(data: Dict, required_keys: List[str]) -> List[str]:
         if key not in data:
             missing_keys.append(key)
     return missing_keys
+
+def check_data_type(data: Dict, types: Dict) -> List[str]:
+    """
+    Check if the data has the correct type
+
+    Args:
+        - data (Dict): The data of the request
+        - types (Dict): The required types
+    Returns:
+        - List[str]: The wrong types
+    """
+    wrong_types = []
+    for key, value in types.items():
+        if key in data and not isinstance(data[key], value):
+            wrong_types.append(key)
+    return wrong_types
