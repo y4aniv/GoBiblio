86d644acda4b597e05d5cb5ac91207c50cbc45a1
Yaniv <yanivdouieb@icloud.com>
2024-11-15 22:37:41 +0100

    Implement login form submission with error handling and loading state

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,8 +2,11 @@
 
 import { Button, PasswordInput, Stack, Text, TextInput, Title } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { notifications } from "@mantine/notifications";
 import Link from "next/link";
+import { useState } from "react";
 
+import apiClient from "@/utils/apiClient";
 import regex from "@/utils/regex";
 
 const AuthLogin = (): React.ReactElement => {
@@ -23,10 +26,56 @@ const AuthLogin = (): React.ReactElement => {
       },
     },
   });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleFormSubmit = (values: Record<string, string>) => {
+    setIsSubmitting(true);
+    apiClient
+      .post("/auth/login", values)
+      .then(() => {
+        // TODO: Handle success
+      })
+      .catch((error) => {
+        switch (error.response.data.message) {
+          case "MISSING_FIELDS":
+            error.response.data.data.forEach((field: string) => {
+              form.setFieldError(field, "Ce champ est requis");
+            });
+            break;
+
+          case "WRONG_DATA_TYPE":
+            error.response.data.data.forEach((field: string) => {
+              form.setFieldError(field, "Type de données incorrect");
+            });
+            break;
+
+          case "INVALID_EMAIL":
+            form.setFieldError("email", "Adresse e-mail invalide");
+            break;
+
+          case "INVALID_CREDENTIALS":
+            form.setFieldError("email", "L'adresse e-mail et/ou le mot de passe est incorrect");
+            break;
+
+          default:
+            notifications.show({
+              title: "Une erreur s'est produite lors de la connexion",
+              message: `Veuillez réessayer plus tard. (C${error.response.status})`,
+              withCloseButton: false,
+            });
+            break;
+        }
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
+  };
+
   return (
     <Stack>
       <Title order={2}>{"Connectez-vous à votre compte"}</Title>
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+      <form onSubmit={form.onSubmit((values) => handleFormSubmit(values))}>
         <Stack>
           <TextInput
             label={"Adresse e-mail"}
@@ -45,7 +94,9 @@ const AuthLogin = (): React.ReactElement => {
               {"Mot de passe oublié ?"}
             </Link>
           </Text>
-          <Button type={"submit"}>{"Connexion"}</Button>
+          <Button type={"submit"} loading={isSubmitting}>
+            {"Connexion"}
+          </Button>
           <Text ta={"center"}>
             {"Vous découvrez GoBiblio ? "} <Link href={"/auth/register"}>{"Créez un compte"}</Link>
           </Text>
