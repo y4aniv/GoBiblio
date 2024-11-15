0d9e462a283420e7d70af9354c34dca9d300de16
Yaniv <yanivdouieb@icloud.com>
2024-11-15 18:45:57 +0100

    Validate email and password formats during user registration; refactor data handling

--- api/routes/auth/register.py
+++ api/routes/auth/register.py
@@ -7,6 +7,8 @@ from classes.user import User
 from bcrypt import hashpw, gensalt
 from classes.session_token import SessionToken
 from datetime import datetime, timedelta
+from utils.regex import regex
+import re
 
 register = Blueprint('register', __name__)
 
@@ -30,33 +32,51 @@ def post_register() -> tuple[Dict[str, Any], int]:
         'email': str,
         'password': str
     }
+    data = {
+        'firstName': request.json.get('firstName').strip(),
+        'lastName': request.json.get('lastName').strip(),
+        'email': request.json.get('email').lower().strip(),
+        'password': request.json.get('password')
+    }
 
-    if missing := find_missing_keys(request.json, required_fields.keys()):
+    if missing := find_missing_keys(data, required_fields.keys()):
         return jsonify({
             'status': 'error',
             'message': 'MISSING_FIELDS',
             'data': missing
         }), HTTPStatus.BAD_REQUEST
 
-    if wrong_types := check_data_type(request.json, required_fields):
+    if wrong_types := check_data_type(data, required_fields):
         return jsonify({
             'status': 'error',
             'message': 'WRONG_DATA_TYPES',
             'data': wrong_types
         }), HTTPStatus.BAD_REQUEST
+    
+    if not re.match(regex['email'], data['email']):
+        return jsonify({
+            'status': 'error',
+            'message': 'INVALID_EMAIL'
+        }), HTTPStatus.BAD_REQUEST
+    
+    if not re.match(regex['password'], data['password']):
+        return jsonify({
+            'status': 'error',
+            'message': 'INVALID_PASSWORD'
+        }), HTTPStatus.BAD_REQUEST
 
     session = Session()
-    if session.query(User).filter_by(email=request.json['email']).first():
+    if session.query(User).filter_by(email=data['email']).first():
         return jsonify({
             'status': 'error',
             'message': 'EMAIL_ALREADY_EXISTS'
         }), HTTPStatus.BAD_REQUEST
 
     user = User(
-        first_name=request.json['firstName'],
-        last_name=request.json['lastName'],
-        email=request.json['email'],
-        password=hashpw(request.json['password'].encode(), gensalt()).decode()
+        first_name=data['firstName'],
+        last_name=data['lastName'],
+        email=data['email'],
+        password=hashpw(data['password'].encode(), gensalt()).decode()
     ).save()
 
     response = jsonify({
@@ -71,6 +91,6 @@ def post_register() -> tuple[Dict[str, Any], int]:
     })
 
     session_token = SessionToken(user_id=user.id, expires_at=datetime.now() + timedelta(days=1)).save()
-    
     response.set_cookie('oneSessionToken', session_token.id, expires=session_token.expires_at, httponly=True)
+
     return response, HTTPStatus.CREATED
